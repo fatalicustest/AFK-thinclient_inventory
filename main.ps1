cb6c04d557eb28ebdca45c530ada55007ef2925d
fatalicus <fatalicus@gmail.com>
2015-07-17 12:05:33 +0200

    Added header
    Added header to main.ps1

---
+++ main.ps1
@@ -0,0 +1,10 @@
+<#
+.SYNOPSIS
+    Script for creating an overview of thin clients and who uses them.
+.DESCRIPTION
+    This script uses Citrix powershell modules to get an overview of all clients currently connected to the citrix
+	environment, and creates a list of clients, with citrix version and user connected.
+.NOTES
+    File Name     : main.ps1
+    Author        : Rolf Røsand (rolf.rosand@akershus-fk.no)
+#>
