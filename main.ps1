469bf718432454a4d2100be3db6d2c7bde62209e
fatalicus <fatalicus@gmail.com>
2015-12-07 14:31:34 +0100

    Added description to main.ps1
    this is just a test description to show Github functionality.

--- main.ps1
+++ main.ps1
@@ -19,3 +19,5 @@ Add-PSSnapIn Citrix.Common.GroupPolicy
 if (Test-Path .\settings.xml){
 [xml]$SettingsFile = Get-Content .\settings.xml
 }
+
+# I'm going to add some more exciting things under here, but for now this is just a test.
